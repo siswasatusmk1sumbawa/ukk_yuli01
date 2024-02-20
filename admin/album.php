ea3afdd372e60c802be54ab4cf8b77a37d8d341f
siswasatu <157083585+siswasatusmk1sumbawa@users.noreply.github.com>
2024-02-21 07:44:53 +0800

    revisi1
    codinganukk

---
+++ admin/album.php
@@ -0,0 +1,66 @@
+<?php 
+session_start();
+if ($_SESSION['status']!='login') {
+  echo "<script>
+  alert('Anda belum login!') ;
+  location.href='../index.php';
+  </script>";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Website Galeri Foto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+       <a href="album.php" class="nav-link">Album</a>
+      </div>
+     
+      <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+      <div class="col-md-4">
+         <div class="card mt-2">
+            <div class="card-header">Tambah Album</div>
+            <div class="card-body">
+                <form action="" method="POST">
+                    <label class="form-label">Nama Album</label>
+
+                </form>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-8">
+
+      </div>
+    </div>
+</div>
+
+
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+    <p>&copy; UKK RPL 2024 | Yuli Astuti</p>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
